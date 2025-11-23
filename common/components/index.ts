d61af5f52b4d9aa22937d126c26543c2057dba59
sharmashiv24251 <sharmashiv24251@gmail.com>
2025-11-23 15:10:31 +0530

    refactor: remove ScoreCard component and its export

--- common/components/index.ts
+++ common/components/index.ts
@@ -6,5 +6,4 @@
 export { default as LoadingPlaceholder } from './LoadingPlaceholder';
 export { default as ErrorPlaceholder } from './ErrorPlaceholder';
 export { default as Button } from './Button';
-export { default as ScoreCard } from './ScoreCard';
 export { default as ScoreCardsRow } from './ScoreCardsRow';
