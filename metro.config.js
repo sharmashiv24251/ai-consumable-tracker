a6a566dfd393420b33374461f2f3988c3204662d
sharmashiv24251 <sharmashiv24251@gmail.com>
2025-11-26 15:22:14 +0530

    chore: simplify NativeWind setup in metro config and `vercel-build` script.

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Only apply NativeWind for native platforms (not during web-only builds)
-if (process.env.EXPO_PUBLIC_PLATFORM !== 'web') {
-  const { withNativeWind } = require('nativewind/metro');
-  module.exports = withNativeWind(config, { input: './global.css' });
-} else {
-  module.exports = config;
-}
+module.exports = withNativeWind(config, { input: './global.css' });
